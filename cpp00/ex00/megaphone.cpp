ea611d1eb2ad10a684208fcefcddc6bdf1b53565
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-11-13 18:34:25 -0300

    cpp00 ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+/* Copyright © 2022 Matheus Oliveira, maolivei A.K.A araggohnxd */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::string str;
+
+    if (argc == 1)
+        str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (size_t i = 1; i < argc; ++i)
+        str.append(argv[i]);
+    for (std::string::iterator c = str.begin(); c != str.end(); ++c)
+        *c = toupper(*c);
+    std::cout << str << std::endl;
+    return (0);
+}
